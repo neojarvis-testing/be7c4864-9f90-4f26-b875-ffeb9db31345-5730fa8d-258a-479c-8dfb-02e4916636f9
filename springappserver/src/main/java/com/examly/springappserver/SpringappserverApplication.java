1c10903f92fcd2d8c817af53e5c36b135a51835f
janith2025 <janithamjc@icloud.com>
2025-05-23 01:19:05 +0000

    api gateway

--- springappserver/src/main/java/com/examly/springappserver/SpringappserverApplication.java
+++ springappserver/src/main/java/com/examly/springappserver/SpringappserverApplication.java
@@ -2,12 +2,24 @@ package com.examly.springappserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class SpringappserverApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringappserverApplication.class, args);
+	}
+
+	@Bean
+	public RouteLocator apiRouteLocator(RouteLocatorBuilder builder){
+
+		return builder.routes()
+		.route("user_route",route->route.path("")
+		.uri("http://localhost:8085"))
+		.build();
 	}
 
 }
