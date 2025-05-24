741cb4dffb49c7c757fc89eb4b6a532106b07ff4
saivulavapati <saivulavapati@gmail.com>
2025-05-24 07:37:54 +0000

    merge commit

--- springapploan/src/main/java/com/examly/springapploan/controller/LoanController.java
+++ springapploan/src/main/java/com/examly/springapploan/controller/LoanController.java
@@ -1,6 +1,15 @@
 package com.examly.springapploan.contorller.LoanController;
 
-@RestController
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestContoller
 public class LoanController {
 
     //get all loans for student and Loan Manager
--- springappserver/src/main/java/com/examly/springappserver/SpringappserverApplication.java
+++ springappserver/src/main/java/com/examly/springappserver/SpringappserverApplication.java
@@ -1,20 +1,6 @@
 package com.examly.springappserver;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-@SpringBootApplication
-public class SpringappserverApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(SpringappserverApplication.class, args);
-	}
-
-}
-
-package com.examly.springappserver;
-
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;  
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
