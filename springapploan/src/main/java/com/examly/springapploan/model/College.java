85f3a48b7636f811359ae850e88afd3caf4136b3
saivulavapati <saivulavapati@gmail.com>
2025-05-25 06:05:56 +0000

    college and college application model created

---
+++ springapploan/src/main/java/com/examly/springapploan/model/College.java
@@ -0,0 +1,63 @@
+package com.examly.springapploan.model;
+
+public class College {
+    private int collegeId;
+    private String collegeName;
+    private String address;
+    private String contactNumber;
+    private String email;
+    private String website;
+    private String courses;
+    private String status;
+
+    public int getCollegeId() {
+        return collegeId;
+    }
+    public void setCollegeId(int collegeId) {
+        this.collegeId = collegeId;
+    }
+    public String getCollegeName() {
+        return collegeName;
+    }
+    public void setCollegeName(String collegeName) {
+        this.collegeName = collegeName;
+    }
+    public String getAddress() {
+        return address;
+    }
+    public void setAddress(String address) {
+        this.address = address;
+    }
+    public String getContactNumber() {
+        return contactNumber;
+    }
+    public void setContactNumber(String contactNumber) {
+        this.contactNumber = contactNumber;
+    }
+    public String getEmail() {
+        return email;
+    }
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    public String getWebsite() {
+        return website;
+    }
+    public void setWebsite(String website) {
+        this.website = website;
+    }
+    public String getCourses() {
+        return courses;
+    }
+    public void setCourses(String courses) {
+        this.courses = courses;
+    }
+    public String getStatus() {
+        return status;
+    }
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    
+}
