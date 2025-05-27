b5cea7298fb233d0f6eb2110e1d4f9404d0ff3ff
janith2025 <janithamjc@icloud.com>
2025-05-27 06:28:41 +0000

    view colleges completed

--- reactapp/src/AdminComponents/ViewColleges.jsx
+++ reactapp/src/AdminComponents/ViewColleges.jsx
@@ -5,7 +5,7 @@ import './ViewColleges.css'
 const ViewColleges = ({token}) => {
  
    const [loading,setLoading] = useState(false);
-   const [data,datas] = useState([]);
+   const [datas,setData] = useState([]);
   
 useEffect(()=> {
 const fetchData = async () => {
@@ -16,7 +16,7 @@ const fetchData = async () => {
             "Authorization":`Bearer ${token}`,
             "Content-Type":"application/json"
         }
-      const response = axios.get(`${baseUrl}/api/colleges`,formData,{ headers });
+      const response = await axios.get(`${baseUrl}/api/colleges`,{ headers });
       setData(response.data);
     }catch(error){
         alert("Failed to Fetch Data:"+error);
@@ -26,7 +26,7 @@ const fetchData = async () => {
     }
 };
 fetchData();
-},[]);
+},[token]);
  
 return (
 
@@ -35,23 +35,38 @@ return (
         <h2>View College</h2> 
         {loading?(
             <p>Loading</p>
-        ) : datas.length ===0 ? (
+        ) : datas.length === 0 ? (
             <p>No Data Found</p>
         ) :
         (
 
             <table>
-            <thead>
-                <tr>
-                    <th>College Id</th>
-                    <th>College Name</th> 
-                </tr>
+            <thead> 
+                <th>College Id</th>
+                    <th>College Name</th>  
+                    <th>Address</th>   
+                    <th>Contact Number</th> 
+                    <th>Email</th> 
+                    <th>website</th> 
+                    <th>courses</th> 
+                    <th>status</th> 
             </thead>
             <tbody>
-            <tr>
-                    <th>{data.collegeId}</th>
-                    <th>{data.collegeName}</th> 
-                </tr>
+          
+
+                {datas.map((college) => (
+                    <tr>
+                    <td>{college.collegeId}</td>
+                    <td>{college.collegeName}</td> 
+                    <td>{college.address}</td> 
+                    <td>{college.contactNumber}</td> 
+                    <td>{college.email}</td> 
+                    <td>{college.website}</td>
+                    <td>{college.courses}</td>
+                    <td>{college.status}</td>
+                    </tr>
+                ))
+                }    
             </tbody>
         </table>
 
